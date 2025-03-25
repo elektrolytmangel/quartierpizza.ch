143fe8a83045ddde433011896a0be7c677ea1936
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-03-25 21:26:56 +0100

    refactor: adjust padding and margin in footer for improved layout

--- web/src/modules/footer/Footer.tsx
+++ web/src/modules/footer/Footer.tsx
@@ -3,14 +3,14 @@ import { LanguageSelection } from "../language-selection/LanguageSelection";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full bg-black text-white mt-10 pt-20 pb-10">
+    <footer className="w-full bg-black text-white mt-10 pt-15 pb-10">
       <div className="flex flex-col items-center justify-center text-center">
         <h3 className="text-2xl">Quartier Pizza</h3>
         <p>Delicious pizza made with love in your neighborhood.</p>
         <p>
           &copy; {new Date().getFullYear()} Quartier Pizza. All rights reserved.
         </p>
-        <p className="text-xl mt-5">Contact Us</p>
+        <p className="text-xl mt-10">Contact Us</p>
         <a href="mailto:info@quartierpizza.ch" className="mb-5">
           info@quartierpizza.ch
         </a>
