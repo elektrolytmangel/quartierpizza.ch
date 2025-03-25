618d8e19908d6e29a04833a02a1e2f6f510dc543
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-03-25 06:58:20 +0100

    refactor: improve layout and styling of Introduction component for better responsiveness

--- web/src/modules/introduction/Introduction.tsx
+++ web/src/modules/introduction/Introduction.tsx
@@ -11,26 +11,31 @@ export const Introduction = (props: Props) => {
   const { t } = useTranslation();
 
   return (
-    <div className="relative w-full flex flex-col md:h-screen">
-      <img
-        src={pizzaNapoOven}
-        alt="Pizza Napo Oven"
-        className="md:w-full md:h-full md:absolute -z-10"
-      />
+    <div className="relative md:w-full flex flex-col md:h-screen bg-gray-100 md:bg-transparent">
+      <div className="w-screen h-full overflow-hidden">
+        <img
+          src={pizzaNapoOven}
+          alt="Pizza Napo Oven"
+          className="object-cover w-full h-full md:absolute -z-10"
+        />
+      </div>
       <div className="top-0 absolute flex flex-col gap-5 justify-center items-center w-full">
         <div className="py-3 md:py-10 px-5 md:px-20  md:m-10 flex flex-col items-center justify-center text-white z-20 md:border md:border-gray-700 md:bg-linear-to-tr md:opacity-90 md:from-black md:to-transparent">
-          <h3 className="text-3xl font-bold font-serif text-center bg-black rounded px-2 py-1 md:bg-transparent">
+          <h3 className="text-3xl font-bold font-serif text-center px-3 py-2 bg-black/40 rounded-full md:bg-transparent">
             {t("lucaAndMarcPresenting")}
           </h3>
           <h1 className="text-6xl mt-4 text-center">Quartier Pizza</h1>
         </div>
-        <p className="text-black md:text-white py-1 px-2 rounded bg-gray-100 md:bg-transparent font-serif text-3xl text-center z-20">
+        <p className="hidden md:flex md:text-white md:bg-transparent font-serif text-3xl text-center z-20">
           {t("pizzaShortIntro")}
         </p>
       </div>
       <div className="hidden md:absolute bottom-5 md:flex justify-center w-full">
         <IconButton icon={iconArrowDown} onClick={props.onNextViewClick} />
       </div>
+      <p className="md:hidden mt-5 mx-5 text-black bg-gray-100 font-serif text-3xl text-center z-20">
+        {t("pizzaShortIntro")}
+      </p>
     </div>
   );
 };
