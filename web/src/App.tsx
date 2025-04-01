09170e7f9f7d9498d6a0dc95eb3381e4a2cf345d
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-04-01 21:28:06 +0200

    feat: add services section with descriptions and images; update translations for improved clarity

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,7 +8,7 @@ import imgPizza from "./assets/images/pizza.jpg";
 import Footer from "./modules/footer/Footer";
 import { Introduction } from "./modules/introduction/Introduction";
 import { TextImageBox } from "./modules/text-image-box/TextImageBox";
-
+import imgServices from "./assets/images/services.jpg";
 function App() {
   const { t } = useTranslation();
   const firstViewRef = useRef<HTMLDivElement>(null);
@@ -24,26 +24,31 @@ function App() {
       <Introduction onNextViewClick={handleNextViewClick} />
       <TextImageBox
         ref={firstViewRef}
+        titel={t("ourServices")}
+        text={t("ourServicesText")}
+        img={imgServices}
+      />
+      <TextImageBox
         titel={t("fireAndDough")}
         text={t("fireAndDoughText")}
         img={imgDough}
+        ltr={true}
       />
       <TextImageBox
         titel={t("ourPizzas")}
         text={t("ourPizzasText")}
         img={imgPizza}
-        ltr={true}
       />
       <TextImageBox
         titel={t("ourValues")}
         text={t("ourValuesText")}
         img={imgIngredients}
+        ltr={true}
       />
       <TextImageBox
         titel={t("contactUs")}
         text={t("contactUsText")}
         img={imgContact}
-        ltr={true}
       />
       <Footer />
     </div>
